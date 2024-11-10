196acc0aab7fc2833fe682a51ff6a867db4b110e
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-10 22:59:06 +0100

    clean console.logS

--- src/components/tic-tac-tow-01/tic-tac-tow-02/Square.jsx
+++ src/components/tic-tac-tow-01/tic-tac-tow-02/Square.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./Square.css";
 
 export default function Square({ value, onClick }) {
-	console.log("function Square");
-
 	return (
 		<button
 			className='square'
