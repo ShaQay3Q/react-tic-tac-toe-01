6efabbe52509ebe1d11d376ede53652ff92ad336
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-10 22:17:56 +0100

    handleClick

--- src/components/tic-tac-tow-01/tic-tac-tow-02/Board.jsx
+++ src/components/tic-tac-tow-01/tic-tac-tow-02/Board.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Board.css";
 import Square from "./Square";
 
 export default function Board() {
+	/** @type {*} */
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	const [isXNext, setIsXNest] = useState(true);
+
+	function handleClick(index) {
+		const squaresCopy = squares.slice();
+		squaresCopy[index] = isXNext ? "X" : "O";
+		setSquares(squaresCopy);
+		setIsXNest(!isXNext);
+	}
+
+	function renderSquare(index) {
+		return (
+			<Square
+				value={Square.index}
+				onClick={() => handleClick(index)}
+			/>
+		);
+	}
 	return (
 		<div className='board'>
 			<div className='board-row'>
