615ee015397dd4547b3c8eaef53cdac373592e06
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-10 17:14:50 +0100

    UI - tic tac toe 02

--- src/components/tic-tac-tow-01/tic-tac-tow-02/tic-tac-toe-02.jsx
+++ src/components/tic-tac-tow-01/tic-tac-tow-02/tic-tac-toe-02.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./tic-tac-toe-02.css";
+import Board from "./Board";
 
 export default function TicTacToe02() {
-	return <h1>Tic Tac Toe 02</h1>;
+	return (
+		<div>
+			<h1>Tic Tac Toe 02</h1>
+			<div className='row'>Status</div>
+			<div className='row'>
+				<button className='restart-button'>Restart</button>
+			</div>
+			<Board />
+		</div>
+	);
 }
